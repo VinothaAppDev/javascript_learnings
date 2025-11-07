6267a463c6b6bfeac198b8b661d597eee8cb53f8
VinothaAppDev <vinotha29022004@gmail.com>
2025-11-07 15:01:00 +0530

    day_2 started

--- day_1/code.js
+++ day_1/code.js
@@ -51,6 +51,35 @@ const student = {
     isPass : true
 };
 
+student.age = 60;
+student["fullName"] = 'aravind';
+
 console.log(student.age);
 console.log(student["age"]);
 console.log(student['age']);
+console.log(student.fullName);
+
+
+//practice
+const flipKartProduct = {
+    name : "Parker Jotter Standard CT Ball Pen (Black)",
+    rating: 4,
+    price: 270,
+    offerPercentage: 5,
+};
+
+console.log(flipKartProduct);
+console.log(typeof flipKartProduct);
+
+const instaProfile = {
+    userName: 'VinothaRamadass',
+    isFollow: true,
+    followers: 569000,
+    following: 234
+};
+
+console.log(instaProfile);
+console.log(typeof instaProfile);
+console.log(typeof instaProfile.followers);
+console.log(typeof instaProfile.isFollow);
+console.log(typeof instaProfile.userName);
