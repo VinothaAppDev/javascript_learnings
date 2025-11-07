9a4572ed3e8cb2bda8f0af24c325997cc5c622a3
VinothaAppDev <vinotha29022004@gmail.com>
2025-11-07 14:35:26 +0530

    object practice

--- day_1/code.js
+++ day_1/code.js
@@ -1,2 +1,56 @@
 console.log("i am vinotha and this is around day 5");
-console.log("its my first day")
+console.log("its my first day");
+// //1
+// {
+//     let a = 10;
+//     console.log("First block:",a);
+// }
+// let a = 11;
+// console.log("global block:",a);
+
+// {
+//     let a = 100;
+//     console.log("second block:",a);
+//     //how can i print 11 in a here...
+// }
+// console.log(a);
+
+// //2
+// {
+//     var a = 10;
+//     console.log("First block:",a);
+// }
+// var a = 11;
+// console.log("global block:",a);
+
+// {
+//     var a = 100;
+//     console.log("second block:",a);
+//     //how can i print 11 in a here...
+// }
+// console.log(a);
+
+
+let name = "vinotha";
+let initial = 'ramadass';
+let multiLine = `
+i
+am
+vinotha
+from
+tiruppur`;
+
+console.log(name);
+console.log(initial);
+console.log(multiLine);
+
+const student = {
+    fullName : "vinotha ramadass",
+    age : 22,
+    cgpa : 8.3,
+    isPass : true
+};
+
+console.log(student.age);
+console.log(student["age"]);
+console.log(student['age']);
---
+++ day_1/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+/2
+// {
+//     var a = 10;
+//     console.log("First block:",a);
+// }
+// var a = 11;
+// console.log("global block:",a);
+
+// {
+//     var a = 100;
+//     console.log("second block:",a);
+//     //how can i print 11 in a here...
+// }
+// console.log(a);
