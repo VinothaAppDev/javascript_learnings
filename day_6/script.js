f35b1bc821dba6f629b53f1ebb34916bd244373e
VinothaAppDev <vinotha29022004@gmail.com>
2025-11-11 16:38:38 +0530

    created a simple webpage to work on

---
+++ day_6/script.js
@@ -0,0 +1,8 @@
+// console.log(window);
+// console.log(window.document)
+// console.log(window.document.html)
+// window.console.log(document)
+console.dir(window.document.body.childNodes[1]);
+
+// go run in browser
+// vs will throw error as the window object is a global object provided by BROWSER
---
+++ day_6/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log(window);
+// console.log(window.document)
+// console.log(window.document.html)
+// win
