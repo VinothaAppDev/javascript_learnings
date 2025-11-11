1181d3ebbbbf4c0facf7cc5ecd63157afbbd987c
VinothaAppDev <vinotha29022004@gmail.com>
2025-11-11 11:30:58 +0530

    callback function

--- day_5/callback_fun.js
+++ day_5/callback_fun.js
@@ -14,3 +14,4 @@ withName("vinotha", hello);         // hello vinotha
 withName("vinotha", hai);           // hai vinotha
 
 // A callback function in JavaScript is a normal function passed as an argument to another function, which is then executed inside that outer function.
+// with traditional function definition of arrow function ...
