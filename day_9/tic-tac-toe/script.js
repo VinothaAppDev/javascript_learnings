49b39e8cfb14aab4a4a8f20d34902fe9b7712fd1
VinothaAppDev <vinotha29022004@gmail.com>
2026-01-04 21:32:40 +0530

    tic-tac-toe game

---
+++ day_9/tic-tac-toe/script.js
@@ -0,0 +1,66 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset");
+let result = document.querySelector(".result");
+let playerO = true;
+
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if(box.innerText !== "")  return;
+    if(playerO){
+      box.innerText = "O";
+      playerO = false;
+    }
+    else{
+      box.innerText ="X";
+      playerO = true;
+    }
+    // box.disabled = true;
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+    for(pattern of winPatterns){
+      // console.log(pattern);
+      let box1 = boxes[pattern[0]].innerText;
+      let box2 = boxes[pattern[1]].innerText;
+      let box3 = boxes[pattern[2]].innerText;
+      if(box1 == "" || box2 == "" || box3 == ""){
+        continue;
+      }
+      if(box1 == box2){
+        if(box2 == box3){
+          result.innerText = `🎊 🎉 ${box1} is the winner 🎉 🎊`;
+          result.style.display = "block";
+          disableAllBoxes();
+          return;
+        }
+      }
+    }
+};
+
+// const disableAllBoxes = () => {
+//   boxes.forEach((box) => {
+//     box.removeEventListener("click");
+//   })
+// };
+
+resetBtn.addEventListener("click", () =>{
+  boxes.forEach((box) => {
+    box.innerText = "";
+    playerO = true;
+    result.style.display = "none";
+  });
+});
+
