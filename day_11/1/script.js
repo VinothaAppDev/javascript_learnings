a09a3738ee116cf272a29dd5e727279f491b1528
VinothaAppDev <vinotha29022004@gmail.com>
2026-01-06 17:20:42 +0530

    object and prototype completed

---
+++ day_11/1/script.js
@@ -0,0 +1,29 @@
+const student = {
+  name: "vinotha",
+  initial: "R",
+  age: 22,
+  yearOfPassOut: 2025,
+  printName(){
+    console.log(`Name is ${this.name}`);
+  },
+  printAge: function (){
+    console.log(`Age is ${this.age}`);
+  },
+  dummyPrint(){
+    console.log(`Student class`);
+  }
+};
+
+const arthy = {
+  name: "Arthy"
+}
+
+const mouli = {
+  name: "moulika",
+  dummyPrint(){
+    console.log(`moulika class`);
+  }
+}
+
+arthy.__proto__ = student;
+mouli.__proto__ = student;
